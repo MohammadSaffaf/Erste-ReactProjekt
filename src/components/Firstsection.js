f04c1472d76307dc1625006f9fe47c63798a98ec
MohammadSaffaf <mhmdsaffha@gmail.com>
2020-11-20 17:07:40 +0100

    Erste-ReactProjekt

---
+++ src/components/Firstsection.js
@@ -0,0 +1,27 @@
+
+const Firstsection = () => {
+    return (    
+        <section className="what_we_do">
+            <div id="idverything">
+                <h3>What We Do</h3>
+                <h1>We've got everything you need to launch and grow your business</h1>
+            </div>
+            <article id="tasks">
+                <div id="brand">
+                    <h2> Brand Identity</h2>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut voluptatum, a dicta fugit maiores debitis ullam illo sapiente aperiam voluptas dolor! Dolorum nisi maiores nulla cupiditate ullam asperiores, veniam nemo.</p>
+                    <h2>Marketing</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum beatae at repellat corrupti porro provident odit illum in ipsa assumenda reprehenderit delectus tenetur ipsum sed voluptatem, nisi quas voluptatum quae?</p>
+                </div>
+                <div id="illustration">
+                    <h2>Illustration</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut similique dolorem deserunt ab, optio ipsam blanditiis voluptas non quis consequuntur nam magnam ducimus, dolore doloribus alias laboriosam tempore, veritatis nisi.</p>
+                    <h2>Web Design</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ex quidem, tenetur totam doloribus cumque aliquid possimus sequi dolor vel neque laudantium corrupti, libero labore quisquam dicta minus voluptate quos?</p>
+                </div>
+            </article>
+        </section>
+     );
+}
+ 
+export default Firstsection;
---
+++ src/components/Head.js
@@ -0,0 +1,33 @@
+const Head = () => {
+    return (    
+        <header className="Glint">
+            <div id="hallo">
+          <h3>Hallo There</h3>
+          <h1>We Are Glint</h1>
+          <p style={{ color: "#fff" }}>________________________________________________________________</p>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor debitis sit porro perspiciatis labore sequi. Obcaecati nemo pariatur laudantium, iure vitae necessitatibus ex? Deserunt iusto molestiae incidunt? Quis, dolores ab, officiis ut vel dolorum officia eos quidem eligendi placeat sit esse odio et. Distinctio rerum sed eligendi optio eos iure eaque laborum, dolor voluptatem necessitatibus, ad enim harum maxime assumenda!</p>
+          
+          </div>
+          <article id="grid">
+          <div id="Award">
+          <h1>127</h1>
+          <h3>Award Received</h3>
+          </div>
+          <div id="Cups">
+          <h1>1505</h1>
+          <h3>Cups of Coffe</h3>
+          </div>
+          <div id="Projects">
+          <h1>109</h1>
+          <h3>Projects Completed</h3>
+          </div>
+          <div id="Happy">
+          <h1>102</h1>
+          <h3>Happy Clients</h3>
+          </div>
+          </article>
+        </header>
+     );
+}
+ 
+export default Head;
