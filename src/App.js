55b39845466cc6d033ef06754fd9e2fea72c419b
MohammadSaffaf <mhmdsaffha@gmail.com>
2020-11-21 17:34:09 +0100

    Footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Head from './components/Head';
 import Firstsection from './components/Firstsection';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <main>
       <Head/>
       <Firstsection/>
+      <Footer/>
     </main>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,92 @@
+const Footer = () => {
+    return ( 
+        <section className="footer">
+<article id="menucarte">
+    <div id="fusce">
+        <figure>
+            <img src="" alt=""/>
+            <figcaption>
+                <h3>Fusce dictum finibus</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis officiis accusantium quidem officia ipsum inventore!</p>
+                <h2>$45/$55</h2>
+            </figcaption>
+        </figure>
+    </div>
+    <div id="aliquam">
+    <figure>
+            <img src="" alt=""/>
+            <figcaption>
+            <h3>Fusce dictum finibus</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis officiis accusantium quidem officia ipsum inventore!</p>
+                <h2>$45/$55</h2>
+            </figcaption>
+        </figure>
+    </div>
+    <div id="sed">
+    <figure>
+            <img src="" alt=""/>
+            <figcaption>
+            <h3>Fusce dictum finibus</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis officiis accusantium quidem officia ipsum inventore!</p>
+                <h2>$45/$55</h2>
+            </figcaption>
+        </figure>
+    </div>
+    <div id="sagittis">
+    <figure>
+            <img src="" alt=""/>
+            <figcaption>
+            <h3>Fusce dictum finibus</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis officiis accusantium quidem officia ipsum inventore!</p>
+                <h2>$45/$55</h2>
+            </figcaption>
+        </figure>
+    </div>
+    <div id="eget">
+    <figure>
+            <img src="" alt=""/>
+            <figcaption>
+            <h3>Fusce dictum finibus</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis officiis accusantium quidem officia ipsum inventore!</p>
+                <h2>$45/$55</h2>
+            </figcaption>
+        </figure>
+    </div>
+    <div id="quisque">
+    <figure>
+            <img src="" alt=""/>
+            <figcaption>
+            <h3>Fusce dictum finibus</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis officiis accusantium quidem officia ipsum inventore!</p>
+                <h2>$45/$55</h2>
+            </figcaption>
+        </figure>
+    </div>
+    <div id="dui">
+    <figure>
+            <img src="" alt=""/>
+            <figcaption>
+            <h3>Fusce dictum finibus</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis officiis accusantium quidem officia ipsum inventore!</p>
+                <h2>$45/$55</h2>
+            </figcaption>
+        </figure>
+    </div>
+    <div id="donec">
+    <figure>
+            <img src="" alt=""/>
+            <figcaption>
+            <h3>Fusce dictum finibus</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis officiis accusantium quidem officia ipsum inventore!</p>
+                <h2>$45/$55</h2>
+            </figcaption>
+        </figure>
+    </div>
+
+</article>
+
+        </section>
+     );
+}
+ 
+export default Footer;
